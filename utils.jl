9bc5ae67b693c889dd724a6ce46d17dc20911d4e
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2021-11-01 20:32:13 +0900

    del unused functions

--- utils.jl
+++ utils.jl
@@ -1,16 +1,50 @@
-function hfun_bar(vname)
-  val = Meta.parse(vname[1])
-  return round(sqrt(val), digits=2)
-end
+function hfun_plutonotebookpage(params)
+    path = params[1]
+    path_to_html = if endswith(path, ".jl")
+        p, _jl = splitext(path)
+        p * ".html"
+    else
+        path * ".html"
+    end
 
-function hfun_m1fill(vname)
-  var = vname[1]
-  return pagevar("index", var)
-end
+    # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy#directives
 
-function lx_baz(com, _)
-  # keep this first line
-  brace_content = Franklin.content(com.braces[1]) # input string
-  # do whatever you want here
-  return uppercase(brace_content)
+    return """
+    <style>
+
+    .content {
+        max-width: 100%;
+        margin-right: 0px;
+        padding: 0px;
+        overflow-y: hidden;
+        height: 100vh;
+    }
+    .franklin-content {
+        padding: 0px;
+    }
+    .page-foot {
+        display: none;
+    }
+    .plutopage {
+        height: 100vh;
+    }
+    .smallscreenlink {
+        display: none;
+    }
+    @media (max-width: 768px) {
+        .franklin-content {
+            padding: 0px;
+        }
+    }
+    </style>
+    
+    <iframe width="100%" height="100%"
+    src="$(path_to_html)"
+    class="plutopage"
+    frameborder="0"
+    allow="accelerometer; ambient-light-sensor; autoplay; battery; camera; display-capture; document-domain; encrypted-media; execution-while-not-rendered; execution-while-out-of-viewport; fullscreen; geolocation; gyroscope; layout-animations; legacy-image-formats; magnetometer; microphone; midi; navigation-override; oversized-images; payment; picture-in-picture; publickey-credentials-get; sync-xhr; usb; wake-lock; screen-wake-lock; vr; web-share; xr-spatial-tracking"
+    allowfullscreen></iframe>
+
+    <a class="smallscreenlink" href="$(path_to_html)"></a>
+    """
 end
