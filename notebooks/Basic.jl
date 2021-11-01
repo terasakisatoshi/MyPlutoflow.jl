f51a6d11fe62facfd06e535b7e6ecaef51610361
terasakisatoshi <terasakisatoshi.math@gmail.com>
2021-11-01 14:55:11 +0000

    Deploying to gh-pages from  @ 3aafb7f4c6d3f80fba2c7a8442ed7384dcc83355 🚀

--- notebooks/Basic.jl
+++ notebooks/Basic.jl
@@ -16,6 +16,10 @@ using InteractiveUtils
 # ╔═╡ 3049a854-3b10-11ec-17c3-2bd1d28fb0d7
 
 
+# ╔═╡ b2dfa629-93d4-44d5-9584-32de8ec3d1c4
+md"""This notebook is taken from [Pluto.jl/sample/Basic.jl](https://github.com/fonsp/Pluto.jl/blob/main/sample/Basic.jl)
+"""
+
 # ╔═╡ b2d786ec-7f73-11ea-1a0c-f38d7b6bbc1e
 md"""
 # The Basel problem
@@ -47,6 +51,7 @@ sqrt(sum(seq) * 6.0)
 # ╟─fae3fa10-3b10-11ec-22ad-8d721eef9e1f
 # ╟─a38cfb68-3b10-11ec-0f31-27172c3a67c7
 # ╟─3049a854-3b10-11ec-17c3-2bd1d28fb0d7
+# ╟─b2dfa629-93d4-44d5-9584-32de8ec3d1c4
 # ╟─b2d786ec-7f73-11ea-1a0c-f38d7b6bbc1e
 # ╠═b2d792c2-7f73-11ea-0c65-a5042701e9f3
 # ╠═b2d79330-7f73-11ea-0d1c-a9aad1efaae1
